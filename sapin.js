ae9aa45698cdf806352cbcf26f95bb3ff9132fdd
Romain Carrillo <carrillo.romain@gmail.com>
2020-12-23 16:43:49 +0100

    version en couleur avec étoile

--- sapin.js
+++ sapin.js
@@ -1,14 +1,30 @@
-var etages = [];
-nbr_etages = prompt ('Combien de niveaux sur votre sapin ?');
+// Déclaration des variables
 
+var etages = [];
 
 cadreGauche = document.getElementById('cadre-1');
 cadreDroite = document.getElementById('cadre-2');
+star = document.getElementById('star');
 
+// Déclaration des fonctions
 
-for (i = 0; i < nbr_etages; i++) {
-    etages.push(i); //remplissage du tableau des étages
-} 
+function Pied() {
+    piedDroit = document.createElement('span');
+    piedDroit.innerHTML = "<span>|</span><br><span>|</span>";
+    piedDroit.className = "pied";
+    cadreDroite.appendChild(piedDroit);
+    piedGauche = document.createElement('span');
+    piedGauche.innerHTML = "<span>|</span><br><span>|</span>";
+    piedGauche.className = "pied";
+    cadreGauche.appendChild(piedGauche);
+}
+
+function Etoile() {
+    etoile = document.createElement('span');
+    etoile.innerHTML = "<span>&#9733</span>";
+    etoile.className = "etoile";
+    star.appendChild(etoile);
+}
 
 function Rang(e) { //le nombre de \ par rang
     rang = "";
@@ -26,6 +42,14 @@ function Etage(e) { //créer les étages en ajoutant le nombre de rangs correpon
     return etage;
 }
 
+// Début du programme
+
+nbr_etages = prompt ('Combien de niveaux sur votre sapin ?');
+
+for (i = 0; i < nbr_etages; i++) {
+    etages.push(i); //remplissage du tableau des étages
+} 
+
 for (let i = 0; i < nbr_etages; i++ ) { //créer le nombre d'étages souhaité
     Etage(i);
     brancheGauche = document.createElement('span');
@@ -36,17 +60,8 @@ for (let i = 0; i < nbr_etages; i++ ) { //créer le nombre d'étages souhaité
     cadreDroite.appendChild(brancheDroite);
 }
 
-function Pied() {
-    piedDroit = document.createElement('span');
-    piedDroit.innerHTML = "<span>|</span><br><span>|</span>";
-    cadreDroite.appendChild(piedDroit);
-    piedGauche = document.createElement('span');
-    piedGauche.innerHTML = "<span>|</span><br><span>|</span>";
-    cadreGauche.appendChild(piedGauche);
-}
-
 Pied();
-
+Etoile();
 
 /*ajouter 1 élements au tableau a chaque nombre d'étage*/
 
